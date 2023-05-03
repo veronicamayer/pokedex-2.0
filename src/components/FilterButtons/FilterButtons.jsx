4bd3c0029783d22db64e2d0c9837c244489a9b8a
veronicamayer <mayerveronica.vm@gmail.com>
2023-05-03 14:22:05 +0200

    improved responsiveness

--- src/components/FilterButtons/FilterButtons.jsx
+++ src/components/FilterButtons/FilterButtons.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import "./FilterButtons.scss";
 
 const FilterButtons = ({
@@ -7,6 +7,20 @@ const FilterButtons = ({
     isLightMode,
     allTypeColors,
 }) => {
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth <= 768);
+        };
+
+        window.addEventListener("resize", handleResize);
+        handleResize();
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     const handleTypeClick = (type) => {
         if (selectedTypes.includes(type)) {
             setSelectedTypes(selectedTypes.filter((t) => t !== type));
@@ -15,22 +29,45 @@ const FilterButtons = ({
         }
     };
 
+    const handleOpenMenu = () => {
+        setIsMenuOpen(true);
+    };
+
+    const handleCloseMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
         <section id="filterButtons" className={isLightMode ? "" : "darkTheme"}>
-            {Object.entries(allTypeColors).map(([type, color]) => (
-                <label
-                    key={type}
-                    className={`labelButton ${
-                        selectedTypes.includes(type) ? "selected" : ""
-                    }`}
-                    style={{ backgroundColor: color }}
-                    onClick={() => handleTypeClick(type)}
-                    htmlFor={type}
-                >
-                    {type}
-                    <input type="checkbox" name={type} />
-                </label>
-            ))}
+            {isSmallScreen && !isMenuOpen ? (
+                <button onClick={handleOpenMenu} className="toggleButton">
+                    <i class="fa-solid fa-bars"></i>
+                </button>
+            ) : null}
+            {isMenuOpen ? (
+                <button onClick={handleCloseMenu} className="toggleButton">
+                    <i class="fa-solid fa-x"></i>
+                </button>
+            ) : null}
+
+            {!isSmallScreen || isMenuOpen ? (
+                <div className="overlay">
+                    {Object.entries(allTypeColors).map(([type, color]) => (
+                        <label
+                            key={type}
+                            className={`labelButton ${
+                                selectedTypes.includes(type) ? "selected" : ""
+                            }`}
+                            style={{ backgroundColor: color }}
+                            onClick={() => handleTypeClick(type)}
+                            htmlFor={type}
+                        >
+                            {type}
+                            <input type="checkbox" name={type} />
+                        </label>
+                    ))}
+                </div>
+            ) : null}
         </section>
     );
 };
